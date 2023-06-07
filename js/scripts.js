d9c1a8260fbda10c2753e890ba0cd2ee9b361a81
Lindsay Warr <iamalion@gmail.com>
2023-06-07 10:11:31 -0700

    Add boldPassage function to handleFormSubmission

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
+//Utility Logic
+function isEmpty(testString) {
+    return (testString.trim().lenghth === 0)
+}
+
 // Business Logic
 function wordCounter(text) {
-    if (text.trim().length === 0) {
+    if (isEmpty(text)) {
         return 0;
     }
     let wordCount = 0;
@@ -14,7 +19,7 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-     if (text.trim().length === 0) {
+     if (isEmpty(word)) {
         return 0;
      }
     const textArray = text.split(" ");
@@ -26,6 +31,27 @@ function numberOfOccurrencesInText(word, text) {
     });
     return wordCount;
 }
+
+function boldPassage(word, text) {
+    if ((isEmpty(text)) || (isEmpty(word))) {
+      return null;
+    }
+    const p = document.createElement("p");
+    let textArray = text.split(" ");
+    textArray.forEach(function(element, index) {
+      if (word === element) {
+        const bold = document.createElement("strong");
+        bold.append(element);
+        p.append(bold);
+      } else {
+        p.append(element);
+      }
+      if (index !== (textArray.length - 1)) {
+        p.append(" ");
+      }
+    });
+    return p;
+  }
 
 
 // function omitOffensiveWords(text) {
@@ -49,6 +75,7 @@ function numberOfOccurrencesInText(word, text) {
 // }
 
 //UI Logic
+
 function handleFormSubmission() {
     event.preventDefault();
     const passage = document.getElementById("text-passage").value;
@@ -57,29 +84,14 @@ function handleFormSubmission() {
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     document.getElementById("total-count").innerText = wordCount;
     document.getElementById("selected-count").innerText = occurrencesOfWord;
+    let boldedPassage = boldPassage(word, passage);
+    if (boldedPassage) {
+        document.querySelector("div#bolded-passage").append(boldedPassage);
+        } else {
+            document.querySelector("div#bolded-passage").innerText = null;
+        }
   }
   
-  function boldPassage(word, text) {
-    if ((text.trim().length === 0) || (word.trim().length === 0)) {
-      return null;
-    }
-    const p = document.createElement("p");
-    let textArray = text.split(" ");
-    textArray.forEach(function(element, index) {
-      if (word === element) {
-        const bold = document.createElement("strong");
-        bold.append(element);
-        p.append(bold);
-      } else {
-        p.append(element);
-      }
-      if (index !== (textArray.length - 1)) {
-        p.append(" ");
-      }
-    });
-    return p;
-  }
-
   window.addEventListener("load", function() {
     document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
   });
